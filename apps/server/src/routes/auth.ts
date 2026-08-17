9ef0bb5ef9027123f1849cbe8556f8693f050c1b
HusseinAbuMariam <huseinmohamaad@gmail.com>
2026-08-17 14:04:25 +0300

    feat(auth): add authentication routes

---
+++ apps/server/src/routes/auth.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { registerController } from "../controllers/auth.controller.js";
+
+const router: Router = Router();
+
+router.post("/register", registerController);
+
+export default router;

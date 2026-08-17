6a1c8d11900ede8a4518cbe39650f7916e26ec9e
HusseinAbuMariam <huseinmohamaad@gmail.com>
2026-08-17 14:36:08 +0300

    feat(auth): implement user registration

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,21 +1,5 @@
 import { env } from "@trello-clone/env/server";
-import cors from "cors";
-import express from "express";
-
-const app = express();
-
-app.use(
-  cors({
-    origin: env.CORS_ORIGIN,
-    methods: ["GET", "POST", "OPTIONS"],
-  }),
-);
-
-app.use(express.json());
-
-app.get("/", (_req, res) => {
-  res.status(200).send("OK");
-});
+import app from "./app.js";
 
 app.listen(env.PORT, () => {
   console.log(`Server is running on http://localhost:${env.PORT}`);

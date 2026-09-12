348fbab9e6abff99deb484558e2c71735ae5ff48
HusseinAbuMariam <huseinmohamaad@gmail.com>
2026-09-12 13:57:25 +0300

    fix(auth): parse session cookies

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { env } from "@trello-clone/env/server";
+import cookieParser from "cookie-parser";
 const app: express.Application = express();
 app.set("trust proxy", 1);
 
@@ -12,6 +13,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cookieParser());
 app.get("/", (_req, res) => {
   res.json({
     success: true,
